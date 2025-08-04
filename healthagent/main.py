6bc7beda3f68e5b1f4115296d880c117fec2d652
Aditi Gaur <aditigaur@microsoft.com>
2025-08-04 10:56:03 -0700

    Add memory profiling for healthagent in DEBUG mode.

--- healthagent/main.py
+++ healthagent/main.py
@@ -1,21 +1,21 @@
 import asyncio
+import os
 import importlib.resources
 import logging
 import logging.config
+import tracemalloc
 from healthagent.healthagent import Healthagent
 
 log = logging.getLogger(__name__)
 
 def main():
-    #TODO: improve signal handling, it isnt quite working as expected just yet.
-    # Potentially add a 30 second timed exit, and perhaps cancellation tokens,
-    # Right now we wait for Async Scheduler to really read the stop_event which depending
-    # on the current task could take some time.
-    #signal.signal(signalnum=signal.SIGTERM, handler=handler)
-    #signal.signal(signalnum=signal.SIGINT, handler=handler)
+
     with importlib.resources.path('healthagent', 'logging.conf') as config_path:
         logging.config.fileConfig(config_path)
-    asyncio.run(Healthagent.run())
+    debug_mode = False
+    if os.getenv("DEBUG_MODE") == "1":
+        debug_mode = True
+    asyncio.run(Healthagent.run(debug_mode=debug_mode))
 
 if __name__ == "__main__":
     main()
